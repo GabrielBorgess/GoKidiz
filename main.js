993e93414f5957507ffd52fca7f89959a0ea9f54
Gabriel-Borges <staffgabs@gmail.com>
2022-12-02 08:48:51 -0300

    finalV

--- main.js
+++ main.js
@@ -10,3 +10,36 @@ window.addEventListener("scroll", () => {
 
     recentScrollY = window.scrollY
 });
+
+
+const scrollOffset = 100;
+
+const scrollElements = document.querySelectorAll('.hidden');
+
+scrollElements.forEach((el, offset = 0) => {
+    const elementTop = el.getBoundingClientRect().top;
+    return (
+        elementTop <= 
+        ((window.innerHeight || document.documentElement.clientHeight) - offset)
+      );
+  })
+
+const displayScrollElement = () => {
+    scrollElements.classList.add('show');
+};
+
+const hideScrollElement = () => {
+    scrollElements.classList.remove('show');
+};
+   
+  const handleScrollAnimation = () => {
+      if (elementInView(scrollElements, scrollOffset)) {
+        displayScrollElement();
+      } else {
+        hideScrollElement();
+      }
+  }
+
+  window.addEventListener('scroll', () => {
+    handleScrollAnimation();
+  })
